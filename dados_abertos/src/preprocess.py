f5756419d2de6efcb35cc94f19a101f822f8a89d
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-05-24 08:20:44 -0300

    refactor: func json_to_csv simplificada

--- dados_abertos/src/preprocess.py
+++ dados_abertos/src/preprocess.py
@@ -4,21 +4,16 @@ from pathlib import Path
 from utils.messages import print_success, print_error
 
 
-def json_to_csv(qtd_atributos: int, cabecalho: str, raw: Path, transformed: Path):
+def json_to_csv(cabecalho: str, raw: Path, transformed: Path):
     if not transformed.is_file():
         Path(transformed).touch()
 
-    qtd_atributos_cabecalho = len(cabecalho.split(";"))
+    qtd_atributos = len(cabecalho.split(";"))
 
     with open(raw) as f:
         loaded_json = json.load(f)
 
     try:
-        if qtd_atributos != qtd_atributos_cabecalho:
-            raise Exception(
-                f"O número de atributos informado é incompatível com a quantidade de atributos presentes no cabeçalho.\nQuantidade atributos: {qtd_atributos}\nAtributos no cabeçalho: {qtd_atributos_cabecalho}"
-            )
-
         with open(transformed, "a") as output_file:
             output_file.write(cabecalho)
 
