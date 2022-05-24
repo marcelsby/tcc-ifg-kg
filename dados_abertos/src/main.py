ea3cbe66bc74eac5f55839bd6b6ac6c7db107be7
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-05-24 08:22:24 -0300

    feat: transformação de unidades do IFG

--- dados_abertos/src/main.py
+++ dados_abertos/src/main.py
@@ -8,15 +8,20 @@ if not transformed_path.is_dir():
     Path(transformed_path).mkdir()
 
 json_to_csv(
-    16,
     "campus;nome;codigo;modalidade;formato;turno;periodo_de_ingresso;qtd_vagas_ano;nivel;ch_disciplinas;ch_complementar;ch_estagio;ch_optativas;ch_projeto_final;ch_total;qtd_semestres\n",
     raw_path / "cursos.json",
     transformed_path / "cursos.csv",
 )
 
 json_to_csv(
-    4,
     "nome;matricula;curso;campus\n",
     raw_path / "docentes.json",
     transformed_path / "docentes.csv",
 )
+
+json_to_csv(
+    "nome;sigla;endereco;site;telefone;email;cnpj;uasg;autoridade_maxima_unidade\n",
+    raw_path / "unidades.json",
+    transformed_path / "unidades.csv",
+)
+
