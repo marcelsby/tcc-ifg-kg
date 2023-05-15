e99b928cd8f7af29c262a293ba6d61af52bfc1ee
Marcel Barbosa <marcel.sby.br@gmail.com>
2023-05-15 08:58:08 -0300

    feat: limpeza do JSON de disciplinas ministradas

--- app/dados_abertos/collected_cleaners/barramento_ifg.py
+++ app/dados_abertos/collected_cleaners/barramento_ifg.py
@@ -28,7 +28,7 @@ def _clean_cursos():
         Transforma os valores que estão representados como float para inteiros, o que é a 
         maneira correta de representá-los.
         """
-        for item_index in range(10, len(curso["itens"]) - 1):
+        for item_index in range(10, len(curso["itens"])):
             value = curso["itens"][item_index]["valor"]
 
             if value is not None:
@@ -39,8 +39,35 @@ def _clean_cursos():
         json.dump(cursos, f, separators=(",", ":"), ensure_ascii=False)
 
 
+def _clean_disciplinas_ministradas():
+    disciplinas_ministradas_json_file = Storage.get_file("dados_abertos/raw/disciplinas_ministradas.json")
+
+    validate_json_file(disciplinas_ministradas_json_file)
+
+    with open(disciplinas_ministradas_json_file, "r") as f:
+        disciplinas_ministradas = json.load(f)
+
+    for disciplina_ministrada in disciplinas_ministradas:
+        """
+        Outro problema encontrado nesse dataset são algumas disciplinas com o nome incompleto e com caracteres de escape de linha,
+        contando que o dicionário de dados especifica 16 campos, quando os itens do dataset foram analisados
+        descobriu-se que na verdade estão presentes 17 campos, entretanto esse novo campo basicamente repete
+        alguns dados já apresentados anteriormente (turma, nome da disciplina e carga horária da disciplina). 
+        
+        No caso das disciplinas que estavam com o nome incompleto e com falhas foi possível consultar seu nome completo nesse campo
+        adicional e corrigir essas inconsistências.
+        """
+        subject_code: int = int(disciplina_ministrada["itens"][10]["valor"])
+
+        if subject_code == 26734:
+            disciplina_ministrada["itens"][12]["valor"] = "Leitura e Produção Textual de Gêneros Acadêmicos (60)"
+
+    with open(disciplinas_ministradas_json_file, "w") as f:
+        json.dump(disciplinas_ministradas, f, separators=(",", ":"), ensure_ascii=False)
+
+
 def clean():
-    clean_funcs: list[Callable] = [_clean_cursos]
+    clean_funcs: list[Callable] = [_clean_cursos, _clean_disciplinas_ministradas]
 
     with alive_bar(len(clean_funcs), title="Cleaning 'Barramento IFG' collected files") as bar:
         for func in clean_funcs:
