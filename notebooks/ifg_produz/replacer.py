99d074857f864bb029c39986d6c0183800206fd5
Marcel Barbosa <marcel.sby.br@gmail.com>
2024-01-11 08:41:51 -0300

    feat: pré-processamento dos datasets de Conferência e Revista

--- notebooks/ifg_produz/replacer.py
+++ notebooks/ifg_produz/replacer.py
@@ -2,11 +2,16 @@ import numpy as np
 
 
 def replace_na_repr_with_nan(df, null_values: list[str] | str, column_name: str):
-    if isinstance(null_values, str):
+    if not isinstance(null_values, list):
         null_values = [null_values]
+
+    prev_col_type = str(df[column_name].dtype)
     
     indexes = df[df[column_name].isin(null_values)].index
 
     df.loc[indexes, column_name] = np.nan
 
+    if prev_col_type.startswith('int'):
+        df[column_name] = df[column_name].astype('Int64')
+    
     return df
