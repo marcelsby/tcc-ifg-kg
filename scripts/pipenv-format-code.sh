dc4bd209374e4eee4f9b5e048044e2e840df0d91
Marcel Barbosa <marcel.sby.br@gmail.com>
2024-01-17 09:17:34 -0300

    feat: autoflake instalado e aplicado no script de formatação

--- scripts/pipenv-format-code.sh
+++ scripts/pipenv-format-code.sh
@@ -10,3 +10,6 @@ autopep8 -r --in-place --max-line-length 120 $SRC_DIRS
 
 echo "Running isort..."
 isort $SRC_DIRS
+
+echo "Running autoflake..."
+autoflake -riv --expand-star-imports --remove-all-unused-imports $SRC_DIRS
